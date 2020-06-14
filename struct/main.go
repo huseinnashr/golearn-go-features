e8f06736a6bb1261899896ce94f5e3d7b3b689f5
Husein <nvdhunter007@gmail.com>
2020-06-14 17:55:24 +0700

    composite type in struct

--- struct/main.go
+++ struct/main.go
@@ -2,16 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Printf("%+v", alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
 
-	var anon person
-	anon.lastName = " 4n0n"
-	fmt.Printf("%+v", anon)
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
