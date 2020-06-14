c6ff48a6a8bd443acc004ca0ce44a8e63c62ff03
Husein <nvdhunter007@gmail.com>
2020-06-14 18:06:25 +0700

    instantiate empty map, insert & delete item on map

--- map/main.go
+++ map/main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ffff",
-	}
+	colors := make(map[int]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#00ffff",
+	// }
+
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
