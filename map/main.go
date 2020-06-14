fdedb1fb09f975eeaed4fd7b57a4a9b0a948f78d
Husein <nvdhunter007@gmail.com>
2020-06-14 18:12:06 +0700

    loop over map

--- map/main.go
+++ map/main.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	colors := make(map[int]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ffff",
+		"white": "#ffffff",
+	}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ffff",
-	// }
+	printMap(colors)
+}
 
-	colors[10] = "#ffffff"
-
-	delete(colors, 10)
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
